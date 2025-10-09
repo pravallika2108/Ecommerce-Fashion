e1d40ad608ef5e85184b06e180c4861cda2185a6
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-09 22:46:31 +0530

    Update index.d.ts

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,11 +1,13 @@
-import * as Express from 'express';
+import multer from "multer";
 
 declare global {
   namespace Express {
+    // Remove Multer if it's causing errors
+    // If you need to type req.files, do:
     interface Request {
       files?: {
         [fieldname: string]: Express.Multer.File[];
-      } | Express.Multer.File[];
+      };
     }
   }
 }
