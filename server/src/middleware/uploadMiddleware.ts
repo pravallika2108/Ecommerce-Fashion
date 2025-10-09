14abe0bf3e2c74964880f64b8308c591fafe5f48
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-09 22:56:07 +0530

    Update uploadMiddleware.ts

--- server/src/middleware/uploadMiddleware.ts
+++ server/src/middleware/uploadMiddleware.ts
@@ -1,29 +1,34 @@
-import multer, { FileFilterCallback } from "multer";
+import multer from "multer";
 import path from "path";
-import { Request } from "express";
 
-// Configure storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+//configure stroage
+const stroage = multer.diskStorage({
+  destination: function (req, file, cb) {
     cb(null, "uploads/");
   },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.filename + "-" + Date.now() + path.extname(file.originalname)
+    );
   },
 });
 
-// File filter
-const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+const fileFilter = (
+  req: any,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
-    cb(new Error("Not an image! Please upload only images."));
+    cb(new Error("Not an mage! Please upload only images."));
   }
 };
 
 export const upload = multer({
-  storage,
-  fileFilter,
+  storage: stroage,
+  fileFilter: fileFilter,
   limits: { fieldSize: 1024 * 1024 * 5 },
 });
 
