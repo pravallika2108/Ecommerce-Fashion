e78b598d7cfc866b309825bd6939e374f6674322
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-11 15:11:30 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,6 +16,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+console.log(FRONTEND_URL)
 const corsOptions = {
  
    origin: FRONTEND_URL, // Use environment variable
