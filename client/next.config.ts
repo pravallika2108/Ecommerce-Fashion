8e85f6d0c2f9a1b7dc9ccf1d0f137a4e03abaeb6
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-12 16:00:28 +0530

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,31 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // apply to all routes
-        headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://ecommerce-fashion-1.onrender.com", // 👈 must match your frontend domain (NOT backend)
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
