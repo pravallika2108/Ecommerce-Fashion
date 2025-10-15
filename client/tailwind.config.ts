eaee26e3fa84db0e4ad0d3386f19a8fd6be44b9f
pravallika2108 <81466991+pravallika2108@users.noreply.github.com>
2025-10-15 12:25:36 +0530

    Update tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -7,6 +7,11 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+ safelist: [
+    
+    "bg-[#ef4444]",
+    
+  ],
   theme: {
   	extend: {
   		colors: {
